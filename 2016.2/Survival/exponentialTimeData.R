20fb808a22a6a42bc3e4a732a5261586e4c832cd
duranvr <victorduran89@gmail.com>
2016-10-18 09:15:41 -0300

    Questions 5 and 7 from exercise list

--- 2016.2/Survival/exponentialTimeData.R
+++ 2016.2/Survival/exponentialTimeData.R
@@ -7,10 +7,11 @@ mean(t)
 
 #Sufficient statistic used for constructing confidence interval
 T <- sum(t)
-
+2*T
 #Exact confidence interval function (Chi-squared)
 IC <- function(x, alpha = .05){
   estML <- mean(x)
+  T <- sum(x)
   U <- (2*T)/(qchisq(alpha/2, df = 2*length(x)))
   L <- (2*T)/(qchisq(1-alpha/2, df = 2*length(x)))
   return(cat("Your maximum likelihood estimation is: ", estML, 
@@ -21,3 +22,17 @@ IC <- function(x, alpha = .05){
 IC(t)
 
 
+#### Q 7 ####
+
+t <- c(74, 57, 48, 29, 502, 12,70, 21, 29, 386, 59, 27, 153, 26, 326)
+
+#Chi-squared exact confidence interval
+U <- (2*sum(t))/qchisq(0.025, 2*length(t))
+L <- (2*sum(t))/qchisq(0.975, 2*length(t))
+
+IC(t)
+
+#LRT Approximation for CI (Normal distribution)
+
+U <- mean(t) + (mean(t)*qnorm(.975))/sqrt(length(t))
+L <- mean(t) - (mean(t)*qnorm(.975))/sqrt(length(t))
